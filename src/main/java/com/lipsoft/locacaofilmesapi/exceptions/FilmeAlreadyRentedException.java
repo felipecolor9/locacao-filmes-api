ffbc9fd48ed3f5d1cd16824b2566e23082747b7a
felipecolor9 <felipe.m199@gmail.com>
2021-10-22 09:16:50 -0300

    Formato de mensagem de erro corrigido.

--- src/main/java/com/lipsoft/locacaofilmesapi/exceptions/FilmeAlreadyRentedException.java
+++ src/main/java/com/lipsoft/locacaofilmesapi/exceptions/FilmeAlreadyRentedException.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 public class FilmeAlreadyRentedException extends Exception {
 
     public FilmeAlreadyRentedException(Long id, String movieName, LocalDateTime dataFimLocacao) {
-        super(String.format("Filme '%s' com id %s já alugado com data para '%s'! Favor, escolher outro", movieName, id));
+        super(String.format("Filme já alugado com data para %s ! Favor, escolher outro \n", dataFimLocacao)
+        + String.format("ID do filme = %s \n", id)
+        + String.format("Nome do filme = %s", movieName));
     }
 }
