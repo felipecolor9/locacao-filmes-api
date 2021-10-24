610f1019f094a56d73ab21efc2819ff57cda5a73
felipecolor9 <felipe.m199@gmail.com>
2021-10-24 12:15:39 -0300

    Simplificação do builder utilizando Lombok.

--- src/main/java/com/lipsoft/locacaofilmesapi/response/MessageResponse.java
+++ src/main/java/com/lipsoft/locacaofilmesapi/response/MessageResponse.java
@@ -1,42 +1,25 @@
 package com.lipsoft.locacaofilmesapi.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Service
+@Builder(builderClassName = "Builder", toBuilder = true)
 public class MessageResponse {
 
-    private Long idObj;
     private String message;
 
-    private MessageResponse(MessageResponseBuilder builder) {
-        this.idObj = builder.idObj;
-        this.message = builder.message;
-    }
 
-    public Long getIdObj() {
-        return idObj;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public static class MessageResponseBuilder {
-        private String message;
-        private Long idObj;
-
-
-        public MessageResponseBuilder() { }
-
-        public MessageResponseBuilder addMessage(String message) {
-            this.message = message;
-            return this;
+    public MessageResponse createMessageResponse(String s, Long id2) {
+        return MessageResponse
+                .builder()
+                .message(s + id2)
+                .build();
         }
-
-        public MessageResponseBuilder addIdObj(Long idObj) {
-            this.idObj = idObj;
-            return this;
-        }
-        public MessageResponse build() {
-            MessageResponse messageResponse = new MessageResponse(this);
-            return messageResponse;
-        }
-    }
 }
