b3a6d63d4e43aa2c0491ad64adab26ca9e7ee506
felipecolor9 <felipe.m199@gmail.com>
2021-10-25 20:53:55 -0300

    Implementando testes com requisição GET.

--- src/test/java/com/lipsoft/locacaofilmesapi/controller/FilmeControllerTest.java
+++ src/test/java/com/lipsoft/locacaofilmesapi/controller/FilmeControllerTest.java
@@ -1,6 +1,6 @@
 package com.lipsoft.locacaofilmesapi.controller;
 
-import com.lipsoft.locacaofilmesapi.entities.Filme;
+import com.lipsoft.locacaofilmesapi.builder.FilmeDTOBuilder;
 import com.lipsoft.locacaofilmesapi.response.MessageResponse;
 import com.lipsoft.locacaofilmesapi.services.FilmeService;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,22 +8,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static com.lipsoft.locacaofilmesapi.utils.JsonConvertionUtils.asJsonString;
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @ExtendWith(MockitoExtension.class)
 public class FilmeControllerTest {
 
     private static final String BASE_URL = "http://localhost:8080";
-    private static final String MOVIE_API_PATH = "/api/filmes";
+    private static final String MOVIE_API_PATH = "/api/filmes/";
     private static final long VALID_MOVIE_ID = 1L;
     private static final long INVALID_MOVIE_ID = 2L;
+
     private MockMvc mockMvc;
 
     @Mock
@@ -40,15 +47,19 @@ public class FilmeControllerTest {
     }
 
     @Test
-    void contextLoads() {
-        assertThat(filmeController).isNotNull();
-        assertThat(filmeService).isNotNull();
-    }
-
-    @Test
     void whenGETIsCalledThenAMovieIsCreated() throws Exception {
+        var filmeDTO = FilmeDTOBuilder.builder().build().toFilmeDTO();
 
-        
+        Mockito.lenient().when(filmeService.add(filmeDTO)).thenReturn(new MessageResponse());
+
+        mockMvc.perform(post(MOVIE_API_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(filmeDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.nomeDoFilme", is(filmeDTO.getNomeDoFilme())))
+                .andExpect(jsonPath("$.anoDeLancamento", is(filmeDTO.getAnoDeLancamento())))
+                .andExpect(jsonPath("$.notaDosUsuarios", is(filmeDTO.getNotaDosUsuarios())))
+                .andExpect(jsonPath("$.notaDaCritica", is (filmeDTO.getNotaDaCritica())));
     }
 
 
