b216725f1fa57b000d6558efeed496738917ed38
felipecolor9 <felipe.m199@gmail.com>
2021-10-25 20:53:31 -0300

    FilmeDTO com builder para fins de testes.

--- src/test/java/com/lipsoft/locacaofilmesapi/builder/FilmeDTOBuilder.java
+++ src/test/java/com/lipsoft/locacaofilmesapi/builder/FilmeDTOBuilder.java
@@ -1,4 +1,32 @@
 package com.lipsoft.locacaofilmesapi.builder;
 
+import com.lipsoft.locacaofilmesapi.dto.FilmeDTO;
+import lombok.Builder;
+
+@Builder
 public class FilmeDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String nomeDoFilme = "Sexta-feira em apuros";
+
+    @Builder.Default
+    private int anoDeLancamento = 1995;
+
+    @Builder.Default
+    private double notaDosUsuarios = 9.7;
+
+    @Builder.Default
+    private double notaDaCritica = 7.8;
+
+    public FilmeDTO toFilmeDTO() {
+        return new FilmeDTO(id,
+                nomeDoFilme,
+                anoDeLancamento,
+                notaDosUsuarios,
+                notaDaCritica);
+    }
+
 }
