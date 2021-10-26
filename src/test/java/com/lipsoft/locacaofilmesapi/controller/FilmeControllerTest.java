e49559afada1751b18fc90b4fa1512c00c162a9e
felipecolor9 <felipe.m199@gmail.com>
2021-10-26 13:22:54 -0300

    Teste com cenário GET válido corrigido; Testes com requisições GET, POST e DELETE criados

--- src/test/java/com/lipsoft/locacaofilmesapi/controller/FilmeControllerTest.java
+++ src/test/java/com/lipsoft/locacaofilmesapi/controller/FilmeControllerTest.java
@@ -1,28 +1,27 @@
 package com.lipsoft.locacaofilmesapi.controller;
 
 import com.lipsoft.locacaofilmesapi.builder.FilmeDTOBuilder;
+import com.lipsoft.locacaofilmesapi.exceptions.FilmeNotFoundException;
 import com.lipsoft.locacaofilmesapi.response.MessageResponse;
 import com.lipsoft.locacaofilmesapi.services.FilmeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import static com.lipsoft.locacaofilmesapi.utils.JsonConvertionUtils.asJsonString;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -35,8 +34,7 @@ public class FilmeControllerTest {
 
     private MockMvc mockMvc;
 
-    @Mock
-    private FilmeService filmeService;
+    FilmeService filmeService = Mockito.mock(FilmeService.class);
     @InjectMocks
     private FilmeController filmeController;
 
@@ -50,33 +48,75 @@ public class FilmeControllerTest {
 
     @Test
     void whenPOSTIsCalledThenAMovieIsCreated() throws Exception {
-        var mockFilme = FilmeDTOBuilder.builder().build().toFilmeDTO();
+        var mockedMovie = FilmeDTOBuilder.builder().build().toFilmeDTO();
 
-        when(filmeService.add(mockFilme)).thenReturn(new MessageResponse());
+        when(filmeService.add(mockedMovie)).thenReturn(new MessageResponse());
         reset(filmeService);
 
         mockMvc.perform(post(MOVIE_API_PATH)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(mockFilme)))
-                .andExpect(status().isCreated());
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(mockedMovie)))
+                .andExpect(status().isCreated())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
-    void whenGETIsCalledThenOkStatusIsReturned() throws Exception {
+    void whenPOSTIsCalledWithoutRequiredFieldThenAnErrorIsReturned() throws Exception {
+        var mockedMovie = FilmeDTOBuilder.builder().nomeDoFilme(null).build().toFilmeDTO();
 
-        var mockFilme = FilmeDTOBuilder.builder().build().toFilmeDTO();
-        when(filmeService.findByID(mockFilme.getId())).thenReturn(mockFilme);
-        reset(filmeService);
-
-        mockMvc.perform(get(MOVIE_API_PATH + "/" + mockFilme.getId())
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nomeDoFilme", is(mockFilme.getNomeDoFilme())))
-                .andExpect(jsonPath("$.anoDeLancamento", is(mockFilme.getAnoDeLancamento())))
-                .andExpect(jsonPath("$.notaDosUsuarios", is(mockFilme.getNotaDosUsuarios())))
-                .andExpect(jsonPath("$.notaDaCritica", is(mockFilme.getNotaDaCritica())));
+        mockMvc.perform(post(MOVIE_API_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(mockedMovie))
+                .characterEncoding("utf-8"))
+                .andExpect(status().isBadRequest());
     }
 
+    @Test
+    void whenGETIsCalledWithValidIdThenOkStatusAndMovieIsReturned() throws Exception {
 
+        var mockedMovie = FilmeDTOBuilder.builder().build().toFilmeDTO();
+        when(filmeService.findByID(mockedMovie.getId())).thenReturn(mockedMovie);
 
+        mockMvc.perform(get(MOVIE_API_PATH + "/" + mockedMovie.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("utf-8"))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(jsonPath("$.nomeDoFilme", is(mockedMovie.getNomeDoFilme())))
+                .andExpect(jsonPath("$.anoDeLancamento", is(mockedMovie.getAnoDeLancamento())))
+                .andExpect(jsonPath("$.notaDosUsuarios", is(mockedMovie.getNotaDosUsuarios())))
+                .andExpect(jsonPath("$.notaDaCritica", is(mockedMovie.getNotaDaCritica())));
+    }
+
+    @Test
+    void whenGETIsCalledWithInvalidIdThenNotFoundExceptionAndStatusIsReturned() throws Exception {
+        var mockedMovie = FilmeDTOBuilder.builder().build().toFilmeDTO();
+        when(filmeService.findByID(INVALID_MOVIE_ID)).thenThrow(new FilmeNotFoundException(INVALID_MOVIE_ID));
+
+        mockMvc.perform(get(MOVIE_API_PATH + "/" + INVALID_MOVIE_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void whenDELETEIsCalledWithValidIdThenNoContentStatusIsReturned() throws Exception {
+
+        var mockedMovie = FilmeDTOBuilder.builder().build().toFilmeDTO();
+        when(filmeService.deleteById(VALID_MOVIE_ID)).thenReturn(new MessageResponse());
+
+        mockMvc.perform(delete(MOVIE_API_PATH + "/" + mockedMovie.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void whenDELETEIsCalledWithInvalidIdThenNotFoundExceptionAndStatusIsReturned() throws Exception {
+
+        var mockedMovie = FilmeDTOBuilder.builder().build().toFilmeDTO();
+        when(filmeService.deleteById(INVALID_MOVIE_ID)).thenThrow(new FilmeNotFoundException(INVALID_MOVIE_ID));
+
+        mockMvc.perform(delete(MOVIE_API_PATH + "/" + INVALID_MOVIE_ID)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
 }
