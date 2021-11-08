1ac7de864fb4bc66e6cd27d865bd216bab813c65
felipecolor9 <felipe.m199@gmail.com>
2021-11-08 16:00:28 -0300

    Adicionado método que requisita do IMDB informações do filme pelo nome e salva em um objeto FilmeDTO.

--- src/main/java/com/lipsoft/locacaofilmesapi/services/FilmeService.java
+++ src/main/java/com/lipsoft/locacaofilmesapi/services/FilmeService.java
@@ -1,63 +1,78 @@
 package com.lipsoft.locacaofilmesapi.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.lipsoft.locacaofilmesapi.dto.FilmeDTO;
-import com.lipsoft.locacaofilmesapi.entities.Filme;
 import com.lipsoft.locacaofilmesapi.exceptions.FilmeNotFoundException;
+import com.lipsoft.locacaofilmesapi.exceptions.InvalidFilmNameInExternalAPIException;
 import com.lipsoft.locacaofilmesapi.mapper.FilmeMapper;
 import com.lipsoft.locacaofilmesapi.repository.FilmeRepository;
 import com.lipsoft.locacaofilmesapi.response.MessageResponse;
-import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@AllArgsConstructor(onConstructor = @__(@Autowired))
-public class FilmeService implements DbBasicsService<FilmeDTO> {
+@Slf4j
+public class FilmeService {
 
+    private final FilmeMapper filmeMapper = FilmeMapper.INSTANCE;
     private FilmeRepository filmeRepository;
     private final MessageResponse messageResponse;
-    private final FilmeMapper filmeMapper = FilmeMapper.INSTANCE;
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
-    @Override
-    public MessageResponse add(FilmeDTO filmeDTO) {
-        var filme = filmeRepository.save(filmeMapper.toModel(filmeDTO));
-        filme.setAtores(new ArrayList<>());
-        return messageResponse.createMessageResponse("Criado o filme com id= ", filme.getId());
+    @Autowired
+    public FilmeService(FilmeRepository filmeRepository, MessageResponse messageResponse, RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
+        this.filmeRepository = filmeRepository;
+        this.messageResponse = messageResponse;
+        this.restTemplate = restTemplateBuilder.build();
+        this.objectMapper = objectMapper;
     }
 
-    @Override
+    public MessageResponse add(String movieName) throws InvalidFilmNameInExternalAPIException {
+        var filmeImdbAPI = requestMovieInfo(movieName);
+        filmeImdbAPI.setElenco(new ArrayList<>());
+        var savedFilme = filmeRepository.save(filmeMapper.toModel(filmeImdbAPI));
+        return messageResponse.createMessageResponse("Criado o filme com id= ", savedFilme.getId());
+    }
+
+    private FilmeDTO requestMovieInfo(String movieName) throws InvalidFilmNameInExternalAPIException {
+        var headers = new HttpHeaders();
+        var gson = new Gson();
+        var url = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"+movieName;
+        headers.add("x-rapidapi-host", "imdb-internet-movie-database-unofficial.p.rapidapi.com");
+        headers.add("x-rapidapi-key", "cc90d47931mshb0e2c470d29ae89p1408c1jsn206319c88e7f");
+        headers.add("Content-Type","application/json");
+        var request = new HttpEntity<String>(headers);
+
+        var response= restTemplate.exchange(url, HttpMethod.GET, request, FilmeDTO.class);
+        var jsonResponse = gson.toJson(response.getBody());
+        var filme = gson.fromJson(jsonResponse, FilmeDTO.class);
+
+        if (filme.getNomeDoFilme().isBlank()) throw new InvalidFilmNameInExternalAPIException(movieName);
+        if (response.getStatusCode() == HttpStatus.OK) log.info("200 OK");
+
+    return response.getBody();
+    }
+
     public List<FilmeDTO> findAll() {
         return filmeRepository.findAll().stream()
                 .map(filmeMapper::toDTO)
                 .collect(Collectors.toList());
     }
 
-    public List<Filme> findAllModel() {
-        return filmeRepository.findAll();
-    }
-
-    @Override
     public FilmeDTO findByID(Long id) throws FilmeNotFoundException {
         return filmeRepository.findById(id).map(filmeMapper::toDTO).orElseThrow(() -> new FilmeNotFoundException(id));
     }
 
-    @Override
-    public MessageResponse update(FilmeDTO filmeDto, Long id) throws FilmeNotFoundException {
-        var filme = filmeRepository.findById(id).orElseThrow(() -> new FilmeNotFoundException(id));
-        filme.setNomeDoFilme(filmeDto.getNomeDoFilme());
-        filme.setAnoDeLancamento(filmeDto.getAnoDeLancamento());
-        filme.setNotaDosUsuarios(filmeDto.getNotaDosUsuarios());
-        filme.setNotaDaCritica(filmeDto.getNotaDaCritica());
-        filmeRepository.save(filme);
-        return messageResponse.createMessageResponse("Atualizado o filme com o id= ", id);
-
-    }
-
-    @Override
     public MessageResponse deleteById(Long id) throws FilmeNotFoundException {
         filmeRepository.findById(id).orElseThrow(() -> new FilmeNotFoundException(id));
         filmeRepository.deleteById(id);
